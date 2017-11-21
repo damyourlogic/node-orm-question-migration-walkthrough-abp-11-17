0b6f79902bbe1b825ba7b8dbe2465ff348ee3743
Amy Jackson <aj81623@gmail.com>
2017-11-21 21:35:12 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -3,7 +3,14 @@
 const db = require("../config/db")
 
 class Question{
-
+  static CreateTable(){
+    retrun new Promise(function(resolve){
+      const sql = `CREATE TABLE questions (id INTEGER PRIMARY KEY)`
+        db.run(sql, function(){
+          resolve("questions table created")
+        })
+    })
+  }
 }
 
 
